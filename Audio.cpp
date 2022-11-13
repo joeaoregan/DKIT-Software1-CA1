c82099cdd4f992ffa2884ddde28efa2f4e09a075
Joe <joeaoregan@gmail.com>
2022-11-13 17:43:40 +0000

    comment audio class

--- Audio.cpp
+++ Audio.cpp
@@ -3,71 +3,70 @@
     Joe O'Regan
     02/11/2022
 */
-#include "Audio.hpp"
-#include <iostream>
-#include <string>
+#include "Audio.hpp" // audio header file
+#include <iostream>  // using cout for debugging
+#include <string>    // easier for me to work with strings
 
-std::string songs[5] = {"song_1", "song_2", "song_3", "song_4", "song_5"};
+std::string songs[5] = {"song_1", "song_2", "song_3", "song_4", "song_5"}; // array of song names
 
-Audio *Audio::s_pInstance = 0;
+Audio *Audio::s_pInstance = 0; // singleton audio instance
 
 void Audio::init()
 {
-    m_musicVolume = 1.0f;
-    m_fxVolume = 1.0f;
-    m_masterVolume = 1.0f;
+    m_musicVolume = 1.0f;  // volume - music
+    m_fxVolume = 1.0f;     // volume - fx
+    m_masterVolume = 1.0f; // volume - master
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++) // add 5 songs
     {
-        std::string src = "resources/music/" + songs[i] + ".mp3";
-        Music song = LoadMusicStream(src.c_str());
-        listOfMusic.push_back(song);
+        std::string src = "resources/music/" + songs[i] + ".mp3"; // string to load each song -- todo - load from file
+        Music song = LoadMusicStream(src.c_str());                // load each song
+        listOfMusic.push_back(song);                              // add to music list
     }
 
-    m_pauseMusic = false; // music starts unpaused
+    m_pauseMusic = false;                        // music starts unpaused
+    m_currentSong = rand() % NUMBER_OF_SONGS;    // select random song each time
+    PlayMusicStream(listOfMusic[m_currentSong]); // play the music
 
-    m_currentSong = rand() % NUMBER_OF_SONGS;
-    // std::cout << "current song index: " << currentSong << std::endl;
-
-    PlayMusicStream(listOfMusic[m_currentSong]);
+    // std::cout << "current song index: " << currentSong << std::endl; // debug message
 }
 
 void Audio::update()
 {
-    UpdateMusicStream(listOfMusic[m_currentSong]);
+    UpdateMusicStream(listOfMusic[m_currentSong]); // update the music stream
 }
 
 void Audio::handleEvents()
 {
     // Restart music playing (stop and play)
-    if (IsKeyPressed(KEY_M))
+    if (IsKeyPressed(KEY_M)) // M key pressed
     {
-        StopMusicStream(listOfMusic[m_currentSong]);
-        PlayMusicStream(listOfMusic[m_currentSong]);
+        StopMusicStream(listOfMusic[m_currentSong]); // stop music stream if playing
+        PlayMusicStream(listOfMusic[m_currentSong]); // play music stream if stopped
     }
-    if (IsKeyPressed(KEY_P))
+    if (IsKeyPressed(KEY_P)) // P key pressed
     {
-        m_pauseMusic = !m_pauseMusic;
+        m_pauseMusic = !m_pauseMusic; // toggle the music paused or playing
 
-        if (m_pauseMusic)
-            PauseMusicStream(listOfMusic[m_currentSong]);
+        if (m_pauseMusic)                                 // if the pause flag is true
+            PauseMusicStream(listOfMusic[m_currentSong]); // pause the music
         else
-            ResumeMusicStream(listOfMusic[m_currentSong]);
+            ResumeMusicStream(listOfMusic[m_currentSong]); // otherwise play the music
     }
 
-    if (IsKeyPressed(KEY_COMMA))
+    if (IsKeyPressed(KEY_COMMA)) // Use comma to skip track back
     {
-        skipTrackBackward();
+        skipTrackBackward(); // skip the current song backwards
     }
-    else if (IsKeyPressed(KEY_PERIOD))
+    else if (IsKeyPressed(KEY_PERIOD)) // use full stop to skip the song forwards
     {
-        skipTrackForward();
+        skipTrackForward(); // skip the current song forwards
     }
 }
 
 void Audio::close()
 {
-    for (unsigned int i = 0; i < listOfMusic.size(); i++)
+    for (unsigned int i = 0; i < listOfMusic.size(); i++) // for each song
     {
         UnloadMusicStream(listOfMusic[i]); // unload each music stream buffers from RAM
     }
@@ -75,6 +74,9 @@ void Audio::close()
     // CloseAudioDevice(); // close the audio device
 }
 
+/*
+    skip the song forwards, or back to start of list if already at end of song list
+*/
 int Audio::skipTrackForward()
 {
     m_currentSong++;                         // select next song index
@@ -85,10 +87,12 @@ int Audio::skipTrackForward()
 
     PlayMusicStream(listOfMusic[m_currentSong]);       // play the song
     SeekMusicStream(listOfMusic[m_currentSong], 0.0f); // start at the beginning
-
-    return m_currentSong;
+    return m_currentSong;                              // return the current song index
 }
 
+/*
+    skip the song backwards, or to the end of song list if at start of song list
+*/
 int Audio::skipTrackBackward()
 {
     m_currentSong--; // select the previous song in the list
@@ -99,58 +103,78 @@ int Audio::skipTrackBackward()
 
     PlayMusicStream(listOfMusic[m_currentSong]);       // play the song
     SeekMusicStream(listOfMusic[m_currentSong], 0.0f); // start at the beginning
-
-    return m_currentSong;
+    return m_currentSong;                              // return the current song index
 }
 
+/*
+    get the percentage of the song that has been played
+*/
 float Audio::timePlayed()
 {
     m_timePlayed = GetMusicTimePlayed(listOfMusic[m_currentSong]) / GetMusicTimeLength(listOfMusic[m_currentSong]);
-    if (m_timePlayed > 1.0f)
+    if (m_timePlayed > 1.0f) // if time played exceed 100%
         m_timePlayed = 1.0f; // Time shouldn't be longer than music length
 
     // std::cout << "time played: " << m_timePlayed << std::endl;
-    return m_timePlayed;
+    return m_timePlayed; // return time played %
 }
 
+/*
+    get name of song that is currently playing
+*/
 const char *Audio::currentTrackName()
 {
-    return songs[m_currentSong].c_str();
+    return songs[m_currentSong].c_str(); // get the song name from the list of song names using the current song as index
 }
 
-void Audio::setMusicVolume(float amount)
+/*
+    set music volume
+    seems to be individual for each song
+*/
+void Audio::setMusicVolume(float amount) // set the music up or down the amount specified
 {
-    if (isVolumeChanging(m_musicVolume, amount))
+    if (isVolumeChanging(m_musicVolume, amount)) // if the volume change flag is set
     {
-        float newVolume = adjustedVolume(m_musicVolume, amount);
-        for (Music song : listOfMusic)
+        float newVolume = adjustedVolume(m_musicVolume, amount); // calculate the new volume
+        for (Music song : listOfMusic)                           // for each song
         {
-            SetMusicVolume(song, newVolume);
+            SetMusicVolume(song, newVolume); // set the new volume
         }
     }
 }
 
-void Audio::setFXVolume(float amount)
+/*
+    set fx volume
+*/
+void Audio::setFXVolume(float amount) // amount = % up or down
 {
-    if (isVolumeChanging(m_fxVolume, amount))
+    if (isVolumeChanging(m_fxVolume, amount)) // if the volume is changing
     {
-        float newVolume = adjustedVolume(m_fxVolume, amount);
-        for (Sound fx : listOfFX)
+        float newVolume = adjustedVolume(m_fxVolume, amount); // calculate the new volume
+        for (Sound fx : listOfFX)                             // for each fx
         {
-            SetSoundVolume(fx, newVolume);
+            SetSoundVolume(fx, newVolume); // set the new volume
         }
     }
 }
 
-void Audio::setMasterVolume(float amount)
+/*
+    set master volume
+*/
+void Audio::setMasterVolume(float amount) // amount = % up or down
 {
-    if (isVolumeChanging(m_masterVolume, amount))
+    if (isVolumeChanging(m_masterVolume, amount)) // if the volume is changing
     {
-        SetMasterVolume(adjustedVolume(m_masterVolume, amount));
+        SetMasterVolume(adjustedVolume(m_masterVolume, amount)); // set the volume
     }
 }
 
-bool Audio::isVolumeChanging(float &volume, float amount)
+/*
+    check is the volume changes
+    if it's already 100% no need to increase
+    if it's already 0% no need to decrease
+*/
+bool Audio::isVolumeChanging(float &volume, float amount) // check will volume change -- amount can be +/- value
 {
     std::cout << "checking volume change" << std::endl;
 
@@ -164,18 +188,22 @@ bool Audio::isVolumeChanging(float &volume, float amount)
     return true; // otherwise change the value
 }
 
-float Audio::adjustedVolume(float &volume, float amount)
+/*
+    adjust the volume
+    keep volume within normal limits
+*/
+float Audio::adjustedVolume(float &volume, float amount) // adjust volume by amount, amount can be +/- value
 {
     volume += amount; // change volume
 
-    if (volume > 1.0f)
+    if (volume > 1.0f) // check volume percentage isn't too high
     {
-        volume = 1.0f; // volume can't be more than 1
+        return 1.0f; // volume can't be more than 1
     }
-    if (volume < 0.0f)
+    if (volume < 0.0f) // check volume percentage isn't too low
     {
-        volume = 0.0f; // volume can't be less than 0
+        return 0.0f; // volume can't be less than 0
     }
 
-    return volume;
+    return volume; // return the new volume
 }
--- Audio.hpp
+++ Audio.hpp
@@ -7,68 +7,64 @@
 
 #define NUMBER_OF_SONGS 5; // total number of songs
 
-const float VOLUME_DOWN = -0.1f;
-const float VOLUME_UP = 0.1f;
+const float VOLUME_DOWN = -0.1f; // % to decrease volume
+const float VOLUME_UP = 0.1f;    // % to increase volume
 
 class Audio
 {
 public:
-    ~Audio() {}
+    ~Audio() {} // constructor
 
-    static Audio *Instance()
+    static Audio *Instance() // singleton audio instance, only one instance of audio will be created and used
     {
-        if (s_pInstance == 0)
+        if (s_pInstance == 0) // if there is no instance of Audio
         {
-            s_pInstance = new Audio();
-            return s_pInstance;
+            s_pInstance = new Audio(); // create a new instance
+            return s_pInstance;        // return that instance
         }
-        return s_pInstance;
+        return s_pInstance; // otherwise return the already created instance
     }
 
-    void init();
-    void update();
-    void handleEvents();
-    void close();
+    void init();         // initialise audio, load files etc.
+    void update();       // update audio streams
+    void handleEvents(); // handle user input
+    void close();        // unload files when done
 
     int skipTrackForward();  // skip song forwards - returns current song index
     int skipTrackBackward(); // skip song backwards - returns current song index
 
-    float timePlayed();
+    float timePlayed(); // percentage of song that has been played
 
-    std::vector<Music> listOfMusic;
-    std::vector<Sound> listOfFX;
+    std::vector<Music> listOfMusic; // list of songs
+    std::vector<Sound> listOfFX;    // list of fx
 
-    const char *currentTrackName();
+    const char *currentTrackName(); // song currently playing
 
-    float getMusicVolume() { return m_musicVolume; }
-    float getFXVolume() { return m_fxVolume; }
-    float getMasterVolume() { return m_masterVolume; }
+    float getMusicVolume() { return m_musicVolume; }   // the current music volume %
+    float getFXVolume() { return m_fxVolume; }         // current fx volume %
+    float getMasterVolume() { return m_masterVolume; } // master volume %
 
-    void setMusicVolume(float amount);
-    void setFXVolume(float amount);
-    void setMasterVolume(float amount);
+    void setMusicVolume(float amount);  // set music volume
+    void setFXVolume(float amount);     // set fx volume
+    void setMasterVolume(float amount); // set master volume
 
 private:
-    Audio() {}
-    static Audio *s_pInstance;
+    Audio() {}                 // private constructor for singleton
+    static Audio *s_pInstance; // audio instance, single Audio instance used in game
 
-    Music m_song1;
-    Music m_song2;
-    Music m_song3;
-    Music m_song4;
-    Music m_song5;
+    Music m_song1, m_song2, m_song3, m_song4, m_song5; // songs
 
-    unsigned int m_currentSong;
-    bool m_pauseMusic;
+    unsigned int m_currentSong; // current song index
+    bool m_pauseMusic;          // pause music flag
 
-    float m_musicVolume;
-    float m_fxVolume;
-    float m_masterVolume;
+    float m_musicVolume;  // % volume for music
+    float m_fxVolume;     // % volume for fx
+    float m_masterVolume; // % volume for master audio
 
-    float m_timePlayed;
+    float m_timePlayed; // % time played
 
-    bool isVolumeChanging(float &volume, float amount);
-    float adjustedVolume(float &volume, float amount);
+    bool isVolumeChanging(float &volume, float amount); // check will the volume change
+    float adjustedVolume(float &volume, float amount);  // adjust the volume, return it's value
 };
 
 #endif
