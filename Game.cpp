045f9a3fc13b65dff0f8de44d6ae41098b7c46dd
Joe <joeaoregan@gmail.com>
2022-11-14 00:54:45 +0000

    comment game

--- Game.cpp
+++ Game.cpp
@@ -6,45 +6,58 @@
     Implements singleton pattern, so only one instace of game active at a time
 */
 
-#include "Game.hpp"
-#include "Menu.hpp"
-#include "InputHandler.hpp"
-#include "Player.hpp"
+#include "Game.hpp"         // This class header file
+#include "Menu.hpp"         // State machine starts game in menu state
+#include "InputHandler.hpp" // handle user input
+#include "Player.hpp"       // Player in Game class, then pass through levels -- todo - implement more than one level -- array of players for multiplayer
 
-Game *Game::s_pGame = 0;
-GameObject *player;
+Game *Game::s_pGame = 0; // one instance of Game exists using singleton pattern
 
+/*
+    initialise the game
+*/
 bool Game::init()
 {
-    m_bRunning = true;
-    bool success = true;
+    m_bRunning = true;   // is the game running
+    bool success = true; // in sdl you get boolean confirmation of textures loading etc. -- todo take this out it's not needed, make init void
 
-    player = new Player();
+    player = new Player(); // player game object
 
-    m_currentGameState = MENU;
+    m_currentGameState = MENU; // the game starts in Menu state
 
-    m_pStateMachine = new StateMachine();
-    m_pStateMachine->change(new Menu());
+    m_pStateMachine = new StateMachine(); // manages state transitions
+    m_pStateMachine->change(new Menu());  // start game in menu state
 
-    return success;
+    return success; // not needed as no checks carried out -- todo - remove or perform initialisation checks
 }
 
+/*
+    update the game state
+*/
 void Game::update(float deltaTime)
 {
-    m_pStateMachine->update(deltaTime);
+    m_pStateMachine->update(deltaTime); // update the current state
 }
 
+/*
+    handle user input
+*/
 void Game::handleEvents()
 {
-    m_pStateMachine->handleInput();
+    m_pStateMachine->handleInput(); // handle user input for current state
 }
 
+/*
+    render the game state
+*/
 void Game::draw()
 {
-    // to do
-    m_pStateMachine->draw();
+    m_pStateMachine->draw(); // render the current state
 }
 
+/*
+    exit the current state
+*/
 void Game::close()
 {
     std::cout << "cleaning game" << std::endl;
@@ -53,5 +66,5 @@ void Game::close()
 
 void Game::reset(int level)
 {
-    // to do
+    // todo -- RESET game / level, clear totals etc
 }
--- Game.hpp
+++ Game.hpp
@@ -5,11 +5,15 @@
 
     Game class
     Implements singleton pattern, so only one instace of game active at a time
+    Handles game state
+    Stores score -- todo implement scores
+    Stores current level
+    Thought this was a good place to store the font so it can be accessed from anywhere -- todo - text manager class with font
 */
 #ifndef GAME_HPP
 #define GAME__HPP
 
-#include "StateMachine.hpp"
+#include "StateMachine.hpp" // Finite State Machine to manage state changes
 
 class Game
 {
@@ -42,11 +46,10 @@ public:
     bool isPaused() { return m_paused; }               // getter: game paused
     void setPaused(bool paused) { m_paused = paused; } // setter: pause the game
 
-    int m_currentGameState; // current game state
+    int m_currentGameState;          // current game state
+    bool exitWindowRequested{false}; // the window exit button was pressed
 
-    bool exitWindowRequested{false};
-
-private:
+private:              // todo - maybe I should make some of these protected
     int m_Score;      // game current score
     int m_Level;      // game current level
     Font m_fontRetro; // font for text
@@ -54,12 +57,17 @@ private:
     static Game *s_pGame; // singleton instance of game
     bool m_bRunning;      // is the game running
 
-    bool m_paused;
+    bool m_paused; // the game is paused -- todo - is this needed if the state machine resumes where level left off by popping / pushing instead of changing statess
 
+    GameObject *player; // The player game object
+
+    /*
+    private constructor - constructor private in singleton pattern
+    */
     Game() : m_Score(0), m_Level(1), m_fontRetro(LoadFont("resources/fonts/Retro.ttf"))
     {
-        m_paused = false;
-    } // constructor
+        m_paused = false; // game is not paused by default
+    }
 };
 
 #endif
